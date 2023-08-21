0edc0f3b0ebe0e59d235ba74ec25f261f1dd9b00
Christopher <Christopher@CACTUAR>
2023-08-21 10:18:19 -0700

    Add project files.

---
+++ HideSides/HideSides.cs
@@ -0,0 +1,38 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenMods;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace KitchenHideSides
+{
+    [UpdateAfter(typeof(GroupHandleChoosingOrder))]
+    [UpdateBefore(typeof(GroupHandleAwaitingOrder))]
+    internal class HideSides : GenericSystemBase, IModSystem
+    {
+        private EntityQuery queryCustomerSettings;
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+
+            queryCustomerSettings = GetEntityQuery(new QueryHelper().All(typeof(CCustomerSettings)));
+        }
+
+        protected override void OnUpdate()
+        {
+            using var groups = queryCustomerSettings.ToEntityArray(Allocator.Temp);
+
+            foreach (var group in groups)
+            {
+                var settings = EntityManager.GetComponentData<CCustomerSettings>(group);
+
+                settings.Ordering.SidesModifier = settings.Ordering.SidesOptional && Main.PrefManager.Get<bool>(Main.HIDE_SIDES_ENABLED)
+                    ? 0f
+                    : OrderingValues.Default.SidesModifier;
+
+                EntityManager.SetComponentData<CCustomerSettings>(group, settings);
+            }
+        }
+    }
+}
---
+++ HideSides/Main.cs
@@ -0,0 +1,80 @@
+﻿using KitchenLib;
+using KitchenMods;
+using PreferenceSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace KitchenHideSides
+{
+    internal class Main : BaseMod, IModSystem
+    {
+        // GUID must be unique and is recommended to be in reverse domain name notation
+        // Mod Name is displayed to the player and listed in the mods menu
+        // Mod Version must follow semver notation e.g. "1.2.3"
+        public const string MOD_GUID = "HideSides.PlateUp.Chrystopher";
+        public const string MOD_NAME = "Hide Sides";
+        public const string MOD_VERSION = "1.0.0";
+        public const string MOD_AUTHOR = "Chrystopher";
+        public const string MOD_GAMEVERSION = ">=1.1.3";
+
+        internal static PreferenceSystemManager PrefManager;
+
+        internal const string HIDE_SIDES_ENABLED = "HideSidesEnabled";
+
+        public Main() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+
+        private HashSet<string> RequiredModNames => new()
+        {
+            "KitchenLib",
+            "PreferenceSystem"
+        };
+
+        protected sealed override void OnPostActivate(Mod mod)
+        {
+            var missingModNames = new List<string>();
+            var loadedModNames = ModPreload.Mods.Select(n => n.Name.ToLowerInvariant());
+            
+            foreach (string name in RequiredModNames)
+            {
+                if (loadedModNames.Contains(name.ToLowerInvariant())) continue;
+                missingModNames.Add(name);
+            }
+            if (missingModNames.Count > 0)
+            {
+                throw new ModPackLoadException($"Error! Missing dependencies. {MOD_NAME} requires that you subscribe to {(String.Join(", ", missingModNames))}.");
+            }
+
+            LogInfo($"{MOD_GUID} v{MOD_VERSION} in use!");
+            CreatePreferences();
+        }
+
+        private static void CreatePreferences()
+        {
+            PrefManager = new PreferenceSystemManager(MOD_GUID, MOD_NAME);
+
+            PrefManager
+            .AddLabel("Hide Sides")
+            .AddInfo("Hide side item orders for customers seated at metal tables?")
+            .AddOption<bool>(
+                HIDE_SIDES_ENABLED,
+                false,
+                new bool[] { false, true },
+                new string[] { "Disabled", "Enabled" })
+            //.AddSpacer()
+            .AddInfo("This setting can be toggled at any time and will take effect for the next customer order.");
+
+            PrefManager.RegisterMenu(PreferenceSystemManager.MenuType.MainMenu);
+            PrefManager.RegisterMenu(PreferenceSystemManager.MenuType.PauseMenu);
+        }
+
+        internal static void LogInfo(string _log) { Debug.Log($"{DateTime.Now} [{MOD_NAME}] " + _log); }
+        internal static void LogWarning(string _log) { Debug.LogWarning($"{DateTime.Now} [{MOD_NAME}] " + _log); }
+        internal static void LogError(string _log) { Debug.LogError($"{DateTime.Now} [{MOD_NAME}] " + _log); }
+        internal static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        internal static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        internal static void LogError(object _log) { LogError(_log.ToString()); }
+    }
+}
